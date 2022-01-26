960c7e2fc446b9d18bb0203f00b2ec2292c91a0e
Ya2001 <30899409+Ya2001@users.noreply.github.com>
2022-01-26 00:01:05 +0000

    Add files via upload

---
+++ src/main/java/ticTacToe/ValueIterationAgent.java
@@ -0,0 +1,202 @@
+package ticTacToe;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//import javax.swing.Action;
+
+/**
+ * A Value Iteration Agent, only very partially implemented. The methods to implement are: 
+ * (1) {@link ValueIterationAgent#iterate}
+ * (2) {@link ValueIterationAgent#extractPolicy}
+ * 
+ * You may also want/need to edit {@link ValueIterationAgent#train} - feel free to do this, but you probably won't need to.
+ * @author ae187
+ *
+ */
+public class ValueIterationAgent extends Agent {
+
+	/**
+	 * This map is used to store the values of states
+	 */
+	Map<Game, Double> valueFunction=new HashMap<Game, Double>();
+	
+	/**
+	 * the discount factor
+	 */
+	double discount=0.9;
+	
+	/**
+	 * the MDP model
+	 */
+	TTTMDP mdp=new TTTMDP();
+	
+	/**
+	 * the number of iterations to perform - feel free to change this/try out different numbers of iterations
+	 */
+	int k=10;
+	
+	
+	/**
+	 * This constructor trains the agent offline first and sets its policy
+	 */
+	public ValueIterationAgent()
+	{
+		super();
+		mdp=new TTTMDP();
+		this.discount=0.9;
+		initValues();
+		train();
+	}
+	
+	
+	/**
+	 * Use this constructor to initialise your agent with an existing policy
+	 * @param p
+	 */
+	public ValueIterationAgent(Policy p) {
+		super(p);
+		
+	}
+
+	public ValueIterationAgent(double discountFactor) {
+		
+		this.discount=discountFactor;
+		mdp=new TTTMDP();
+		initValues();
+		train();
+	}
+	
+	/**
+	 * Initialises the {@link ValueIterationAgent#valueFunction} map, and sets the initial value of all states to 0 
+	 * (V0 from the lectures). Uses {@link Game#inverseHash} and {@link Game#generateAllValidGames(char)} to do this. 
+	 * 
+	 */
+	public void initValues()
+	{
+		
+		List<Game> allGames=Game.generateAllValidGames('X');//all valid games where it is X's turn, or it's terminal.
+		for(Game g: allGames)
+			this.valueFunction.put(g, 0.0);
+		
+		
+		
+	}
+	
+	
+	
+	public ValueIterationAgent(double discountFactor, double winReward, double loseReward, double livingReward, double drawReward)
+	{
+		this.discount=discountFactor;
+		mdp=new TTTMDP(winReward, loseReward, livingReward, drawReward);
+	}
+	
+	/**
+	 
+	
+	/*
+	 * Performs {@link #k} value iteration steps. After running this method, the {@link ValueIterationAgent#valueFunction} map should contain
+	 * the (current) values of each reachable state. You should use the {@link TTTMDP} provided to do this.
+	 * 
+	 *
+	 */
+	public void iterate()
+	{
+		/*I am using the 4 loops to respectively
+		1st: go through all the available states using the keySet method of the valueFunction
+		2nd : to access all possible moves from the state we are currently on (st)
+		3rd : to use the game st and move stm to create a list of all transitions and then iterate through this
+		4th : to get the transition probabilities, living reward , discount and current value to calculate the value of the next state **/
+		double big = -100;
+		for (int i = 0; i < k; i++){
+			for (Game st: valueFunction.keySet()){
+				for(Move stm : st.getPossibleMoves()){
+					double cumSum = 0;
+					List<TransitionProb> tran = mdp.generateTransitions(st, stm);
+					for(TransitionProb t: tran){
+						double val = t.prob* (t.outcome.localReward + (discount * valueFunction.get(t.outcome.sPrime)));	
+						cumSum = cumSum + val;
+						if (cumSum > big){
+							big = cumSum;
+						}
+					}
+						
+				}
+				if (st.isTerminal()){
+					valueFunction.put(st,0.0);
+				}
+				else{
+					valueFunction.put(st, big);
+				}
+			}
+			
+		}
+		
+		
+			
+	}
+	
+	/**This method should be run AFTER the train method to extract a policy according to {@link ValueIterationAgent#valueFunction}
+	 * You will need to do a single step of expectimax from each game (state) key in {@link ValueIterationAgent#valueFunction} 
+	 * to extract a policy.
+	 * 
+	 * @return the policy according to {@link ValueIterationAgent#valueFunction}
+	 */
+	public Policy extractPolicy()
+	{
+		HashMap<Game, Move> pol=new HashMap<Game, Move>();
+		for (Game st: valueFunction.keySet()){
+			for(Move stm : st.getPossibleMoves()){
+					pol.put(st, stm);
+			}
+		}
+		
+		Policy gamePol = new Policy(pol);
+		return gamePol;
+	}
+
+	/**
+	 * This method solves the mdp using your implementation of {@link ValueIterationAgent#extractPolicy} and
+	 * {@link ValueIterationAgent#iterate}. 
+	 */
+	public void train()
+	{
+		/**
+		 * First run value iteration
+		 */
+		this.iterate();
+		/**
+		 * now extract policy from the values in {@link ValueIterationAgent#valueFunction} and set the agent's policy 
+		 *  
+		 */
+		
+		super.policy=extractPolicy();
+		
+		if (this.policy==null)
+		{
+			System.out.println("Unimplemented methods! First implement the iterate() & extractPolicy() methods");
+			//System.exit(1);
+		}
+		
+		
+		
+	}
+
+	public static void main(String a[]) throws IllegalMoveException
+	{
+		//Test method to play the agent against a human agent.
+		ValueIterationAgent agent=new ValueIterationAgent();
+		HumanAgent d=new HumanAgent();
+		
+		Game g=new Game(agent, d, d);
+		g.playOut();
+		
+		
+		
+
+		
+		
+	}
+}
